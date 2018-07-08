75e31e48adb923983e2d299703fa0b9b0c9842f9
Shaun McKinnon <shaun@firesideagency.ca>
2018-07-08 15:41:06 -0400

    Removed excess garbage

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'Bob\'s Bait and Tackle Shop',
-    cards: [
-      {
-        cardTitle: 'Card One',
-        cardBody: 'Boorakacha',
-        cardLink: {
-          linkLabel: 'To Example One',
-          linkHREF: 'http://example.com'
-        }
-      },
-      {
-        cardTitle: 'Card Two',
-        cardBody: 'Poppalooey',
-        cardLink: {
-          linkLabel: 'To Example Two',
-          linkHREF: 'http://example.com'
-        }
-      }
-    ]
+    title: 'Bob\'s Bait and Tackle Shop'
   });
 });
 
